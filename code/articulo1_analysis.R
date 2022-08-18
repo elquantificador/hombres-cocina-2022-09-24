f68262a9c846cfa55f81fb9968292af5673128fe
christianjaviersalasmarquez <salasmarquez2000statistics@gmail.com>
2022-08-17 21:07:29 -0500

    Código con los cálculos y gráficos correspondientes

---
+++ code/articulo1_analysis.R
@@ -0,0 +1,287 @@
+## --------------------------------------------------------------------------- ##
+## ----------------------------- Directorio ---------------------------------- ##
+## --------------------------------------------------------------------------- ##
+
+current.path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(current.path)
+setwd("..")
+
+
+## --------------------------------------------------------------------------- ##
+## ------------------------------ Librerías ---------------------------------- ##
+## --------------------------------------------------------------------------- ##
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(haven)) install.packages("haven", repos = "http://cran.us.r-project.org")
+if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+if(!require(radiant.data)) install.packages("radiant.data", repos = "http://cran.us.r-project.org")
+if(!require(gridExtra)) install.packages("gridExtra", repos = "http://cran.us.r-project.org")
+if(!require(grid)) install.packages("grid", repos = "http://cran.us.r-project.org")
+if(!require(foreign)) install.packages("foreign", repos = "http://cran.us.r-project.org")
+if(!require(survey)) install.packages("survey", repos = "http://cran.us.r-project.org")
+if(!require(srvyr)) install.packages("srvyr", repos = "http://cran.us.r-project.org")
+
+
+## --------------------------------------------------------------------------- ##
+## ------------------------------- Source ------------------------------------ ##
+## --------------------------------------------------------------------------- ##
+
+
+source('code/articulo1_download.R')
+
+
+## --------------------------------------------------------------------------- ##
+## --------------------- Diseño Muestral Encuestas --------------------------- ##
+## --------------------------------------------------------------------------- ##
+
+
+# Librería 'srvyr'
+
+UT2012_survey_design <- UT15_UT2012 %>%
+  as_survey_design(ids = id_upm,
+                   strata = dominio,
+                   weights = fexp,
+                   nest = T)
+
+UT2019_survey_design <- S51P2_UT2019 %>%
+  as_survey_design(ids = upm,
+                   strata = estrato,
+                   weights = fexp,
+                   nest = T)
+
+# Promedio de horas semanales en cocinar o preparar alimentos de los ecuatorianos en el 2012
+
+# No se consideran los NA values porque la pregunta UT15 está dirigida a informantes de 12 en adelante. Los NA values están conformados por los informantes que tienen menos de 12.
+
+df_t_cocina_UT2012_plot1 <- UT2012_survey_design %>% 
+  group_by(sexo) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2012_plot2 <- UT2012_survey_design %>% 
+  group_by(edad_rango) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2012_plot3 <- UT2012_survey_design %>% 
+  group_by(edad_rango, sexo) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2012_plot4 <- UT2012_survey_design %>% 
+  group_by(prov) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2012_plot5 <- UT2012_survey_design %>% 
+  group_by(prov, sexo) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+# Promedio de horas semanales en cocinar o preparar alimentos en 2019 por sexo. 
+
+df_t_cocina_UT2019_plot1 <- UT2019_survey_design %>% 
+  group_by(sexo) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2019_plot2 <- UT2019_survey_design %>% 
+  group_by(edad_rango) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2019_plot3 <- UT2019_survey_design %>% 
+  group_by(edad_rango, sexo) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2019_plot4 <- UT2019_survey_design %>% 
+  group_by(prov) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+df_t_cocina_UT2019_plot5 <- UT2019_survey_design %>% 
+  group_by(prov, sexo) %>%
+  summarise(media = survey_mean(t_horas_cocina, 
+                                vartype = c("se"), 
+                                na.rm = T))
+
+
+## --------------------------------------------------------------------------- ##
+## ------------------------------- Gráficos ---------------------------------- ##
+## --------------------------------------------------------------------------- ##
+
+
+
+plot1_articulo1 <- grid.arrange(
+  
+  ggplot( df_t_cocina_UT2012_plot1 , aes(x = sexo , y = media, fill = sexo )) + 
+    geom_bar(stat = 'identity', width = 0.5, position = 'dodge') + 
+    geom_errorbar(aes(ymin = media - 1.96*media_se, ymax = media + 1.96*media_se), width = 0.2) +
+    scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
+    ylim(0,8.5) +
+    labs(title = "2012", caption = "\n \n Fuente: Encuesta Específica de Uso del Tiempo EUT 2012", fill = "Sexo") +
+    geom_text(aes(label = round(media, digits = 2)  ), color ="black", size = 4,position = position_dodge(1),vjust = -1.4) +
+    xlab("Sexo") +
+    ylab(" ") +
+    theme_classic() +
+    theme(plot.title = element_text(colour = "grey20")) +
+    theme(plot.caption = element_text(colour = "grey30")) +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.text.y = element_blank()) +
+    theme(axis.ticks.y = element_blank()) +
+    theme(axis.line = element_line(colour = 'grey60')) +
+    theme(legend.position='none'),
+  
+  ggplot( df_t_cocina_UT2019_plot1 , aes(x = sexo , y = media, fill = sexo )) + 
+    geom_bar(stat = 'identity', width = 0.5, position = 'dodge') + 
+    geom_errorbar(aes(ymin = media - 1.96*media_se, ymax = media + 1.96*media_se), width = 0.2) +
+    scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
+    ylim(0,8.5) +
+    labs(title = "2019", caption = "\n \n Fuente: Encuesta Multipropósito 2019", fill = "Sexo") +
+    geom_text(aes(label = round(media, digits = 2)  ), color ="black", size = 4,position = position_dodge(1),vjust = -1.4) +
+    xlab("Sexo") +
+    ylab(" ") +
+    theme_classic() +
+    theme(plot.title = element_text(colour = "grey20")) +
+    theme(plot.caption = element_text(colour = "grey30")) +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.text.y = element_blank()) +
+    theme(axis.ticks.y = element_blank()) +
+    theme(axis.line = element_line(colour = 'grey60')) +
+    theme(legend.position= c(0.84,0.95)) +
+    theme(legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="grey30")),
+  
+  ncol=2,
+  
+  top = textGrob("Tiempo semanal promedio en cocinar o preparar alimentos (por sexo) en 2012 y 2019 (horas) \n", gp=gpar(fontsize=14,col="grey20",font=1)),
+  
+  bottom= textGrob("\n Nota: Promedio de horas semanales", gp = gpar(fontsize=10,col="grey30",font=1),x = 0.116))
+
+
+ggsave("images/grafico1-articulo1.png", device = "png", width = 12.5, height = 7, dpi = 900)
+
+
+plot2_articulo1 <- grid.arrange(
+  
+  ggplot( df_t_cocina_UT2012_plot3 %>% filter(edad_rango != 'Edad entre 0 y 11 años'), aes(x = media , y = edad_rango , fill = sexo )) + 
+    geom_bar(stat = 'identity', position = 'dodge') + 
+    geom_errorbar(aes(xmin = media - 1.96*media_se, xmax = media + 1.96*media_se), position = position_dodge(0.9), width = 0.2) +
+    scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
+    xlim(0,12.5) +
+    labs(title = "2012", caption = "\n \n Fuente: Encuesta Específica de Uso del Tiempo EUT 2012", fill = "Sexo") +
+    geom_text(aes(label = round(media, digits = 2)  ), color ="black", size = 4,position = position_dodge(0.9), hjust = -0.8) +
+    xlab("Promedio de horas") +
+    ylab("Grupo de edad") +
+    theme_classic() +
+    theme(plot.title = element_text(colour = "grey20")) +
+    theme(plot.caption = element_text(colour = "grey30")) +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.title.y = element_blank()) +
+    theme(axis.ticks.y = element_blank()) +
+    theme(axis.line = element_line(colour = 'grey60')) +
+    theme(legend.position='none'),
+  
+  ggplot( df_t_cocina_UT2019_plot3 , aes(x = media , y = edad_rango , fill = sexo )) + 
+    geom_bar(stat = 'identity', position = 'dodge') + 
+    geom_errorbar(aes(xmin = media - 1.96*media_se, xmax = media + 1.96*media_se), position = position_dodge(0.9), width = 0.2) +
+    scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
+    xlim(0,12.5) +
+    labs(title = "2019", caption = "\n \n Fuente: Encuesta Multipropósito 2019", fill = "Sexo") +
+    geom_text(aes(label = round(media, digits = 2)  ), color ="black", size = 4,position = position_dodge(0.9), hjust = -0.8) +
+    xlab("Promedio de horas") +
+    ylab("Grupo de edad") +
+    theme_classic() +
+    theme(plot.title = element_text(colour = "grey20")) +
+    theme(plot.caption = element_text(colour = "grey30")) +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.title.y = element_blank()) +
+    theme(axis.ticks.y = element_blank()) +
+    theme(axis.line = element_line(colour = 'grey60')) +
+    theme(legend.position= c(0.8,1)) +
+    theme(legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="grey30")),
+  
+  ncol=2,
+  
+  top = textGrob("Tiempo semanal promedio en cocinar o preparar alimentos (por grupo de edad y por sexo) 2012 vs 2019 (horas) \n", gp=gpar(fontsize=14,col="grey20",font=1)),
+  
+  bottom= textGrob("\n Nota: Promedio de horas semanales", gp = gpar(fontsize=10,col="grey30",font=1),x = 0.218))
+
+
+ggsave("images/grafico2-articulo1.png", device = "png", width = 12.5, height = 7, dpi = 900)
+
+
+plot3_articulo1 <- grid.arrange(
+  
+  ggplot( df_t_cocina_UT2012_plot5 %>% filter(prov == 'Pichincha' | prov == 'Guayas' | prov == 'Azuay'), aes(x = prov, y = media, fill = sexo)) + 
+    geom_bar(stat = 'identity', position = 'dodge') + 
+    geom_errorbar(aes(ymin = media - 1.96*media_se, ymax = media + 1.96*media_se), position = position_dodge(0.9), width = 0.2) +
+    scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
+    ylim(0,8) +
+    labs(title = "2012", caption = "\n \n Fuente: Encuesta Específica de Uso del Tiempo EUT 2012", fill = "Sexo") +
+    geom_text(aes(label = round(media, digits = 2)  ), color ="black", size = 4,position = position_dodge(0.9), vjust = -2.2) +
+    xlab("Provincia") +
+    ylab(" ") +
+    theme_classic() +
+    theme(plot.title = element_text(colour = "grey20")) +
+    theme(plot.caption = element_text(colour = "grey30")) +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.text.y = element_blank()) +
+    theme(axis.ticks.y = element_blank()) +
+    theme(axis.line = element_line(colour = 'grey60')) +
+    theme(legend.position='none'),
+  
+  ggplot( df_t_cocina_UT2019_plot5 %>% filter(prov == 'Pichincha' | prov == 'Guayas' | prov == 'Azuay') , aes(x = prov  , y = media , fill = sexo )) + 
+    geom_bar(stat = 'identity', position = 'dodge') + 
+    geom_errorbar(aes(ymin = media - 1.96*media_se, ymax = media + 1.96*media_se), position = position_dodge(0.9), width = 0.2) +
+    scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
+    ylim(0,8) +
+    labs(title = "2019", caption = "\n \n Fuente: Encuesta Multipropósito 2019", fill = "Sexo") +
+    geom_text(aes(label = round(media, digits = 2)  ), color ="black", size = 4,position = position_dodge(0.9), vjust = -2.6) +
+    xlab("Provincia") +
+    ylab(" ") +
+    theme_classic() +
+    theme(plot.title = element_text(colour = "grey20")) +
+    theme(plot.caption = element_text(colour = "grey30")) +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.text.y = element_blank()) +
+    theme(axis.ticks.y = element_blank()) +
+    theme(axis.line = element_line(colour = 'grey60')) +
+    theme(legend.position= c(0.87,1)) +
+    theme(legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="grey30")),
+  
+  ncol=2,
+  
+  top = textGrob("Tiempo semanal en cocinar o preparar alimentos, por provincia, por sexo 2012 vs 2019 (horas) \n", gp=gpar(fontsize=14,col="grey20",font=1)),
+  
+  bottom= textGrob("\n Nota: Promedio de horas semanales", gp = gpar(fontsize=10,col="grey30",font=1),x = 0.118))
+
+
+ggsave("images/grafico3-articulo1.png", device = "png", width = 12.5, height = 7, dpi = 900)
+
+
+
